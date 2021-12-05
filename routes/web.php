03c8683d49d3f102872b3dc22bd6e4cc4535531f
sarabbany <mdarifrabbani279349@gmail.com>
2021-12-05 10:50:56 +0600

    profile page setup

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/profile', function () {
+    return view('deshboard');
+});
 
 Route::get('userdeshboard', function(){
     return view('Affiliator_dashboard');
